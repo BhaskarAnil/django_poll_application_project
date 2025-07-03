3fd19363eb72a54709305724642036c6187a01a1
Bhaskar anil <jeenubhaskar@gmail.com>
2025-07-03 12:13:40 +0530

    poll app

--- poll_app/settings.py
+++ poll_app/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zfu@k64t#5ih6uz7lhfej9xe!t2t3siep&u!&80vifa(jl@t4a'
+SECRET_KEY = os.getenv('KEY') # Replace with your own secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
